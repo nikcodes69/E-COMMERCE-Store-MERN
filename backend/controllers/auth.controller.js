80a5519f414007feac1d4281122b2ae53e5b21a5
nikhil <nikhil@kali.kalo>
2025-04-11 22:11:46 +0545

    feat(auth): implement login and logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -69,17 +69,54 @@ export const signup = async (req,res)=>{
     }
 
     catch(error){
-        console.log('noo');
-        
+        console.log('Error in login controller',error.message);
         res.status(500).json({message: error.message});
     }
 }
 
 export const login = async (req,res)=>{
-    res.send('Login route called');
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+
+        if(user && (await user.comparePassword(password))){
+            const {accessToken,refreshToken} = generateTokens(user._id);
+             
+            await storeRefreshToken(user._id,refreshToken)
+            setCookies(res,accessToken,refreshToken)
+
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            });
+        }   
+        else{
+            res.status(401).json({message: 'Invalid email or password'});
+        }
+    }
+
+    catch(error){
+        console.log('Error in login controller',error.message);
+        res.status(500).json({message: error.message})
+    }
 }
 
 export const logout = async (req,res)=>{
-    res.send('Logout route called');
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(refreshToken){
+            const decoded = jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token:${decoded.userId}`)
+        }
+        res.clearCookie('accesToken');
+        res.clearCookie('refreshToken');
+        res.json({message: 'Logged out successfully'});
+    }
+        
+    catch (error) {
+        res.status(500).json({message: 'Server error',error: error.message});
+    }
 }
 
